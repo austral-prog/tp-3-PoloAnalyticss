eee7bf1c0b3493392afe4a2acd76ac622c8e8988
PoloAnalyticss <hmaggi@mail.austral.edu.ar>
2025-04-07 12:08:52 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,9 +1,13 @@
 def slice_simple():
     texto = "Awesome"
+    
     letras3 = texto[:3].lower()
     print(letras3)
-    otras3 = texto[2:5]
+    
+    otras3 = texto[2:5].lower
     print(otras3)
-    primeraparte = texto[:4]
-    segundaparte = texto[-3:]
+    
+    primeraparte = texto[:4].lower
+    segundaparte = texto[-3:].lower
     print(primeraparte + segundaparte)
+slice_simple()
