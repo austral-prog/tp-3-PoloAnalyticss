0c53287f8f22ec19922fe7ae33253743390e5439
PoloAnalyticss <hmaggi@mail.austral.edu.ar>
2025-04-07 12:01:15 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,4 +1,8 @@
 def slice_advanced():
+    texto = txt[4::2]
+    print(texto)
+txt = input("ingrese palabra:")
+slice_advanced()
     # Código a implementar utilizando input.
 
 # Para verificar este ejercicio ejecutar el comando
